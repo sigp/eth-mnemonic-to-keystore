6fd6d0aad7b0f7eef6b7a0bed5708a98bf7641dc
Paul Hauner <paul@paulhauner.com>
2025-03-25 05:52:28 +1100

    Initial working commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Eth-Mnemonic-to-Keystore (`emtk`)
+
+A simple CLI utility for generated an encrypted keystore from a mnemonic.
+
+## Features
+
+- Based on [`alloy`](https://docs.rs/alloy/latest/alloy/).
+- Minimal dependencies.
+- Builds a stand-alone binary, ideal for air-gapped generation.
+
+## Limitations
+
+- Only supports English mnemonics. Submit a PR for more languages.
+
+## Usage
+
+### 1. Install the binary to your `~/.cargo/bin` (or equivalent)
+
+```bash
+cargo install --path emtk
+```
+
+### 2. Generate a keystore
+
+```
+emtk --index 0 \
+    --password meow \
+    --output-dir ./ \
+    --mnemonic "wear crucial left gorilla zebra child similar salmon seed survey artwork invite digital giggle enjoy"
+```
+
+The keystore will be save to the `--output-dir` with a UUID filename (*without*
+a `.json` extension).
---
+++ emtk/src/main.rs
@@ -0,0 +1,66 @@
+use alloy_signer_local::{coins_bip39::English, MnemonicBuilder, PrivateKeySigner};
+use clap::Parser;
+use rand::thread_rng;
+use std::path::PathBuf;
+use std::process::exit;
+use std::str::FromStr;
+use zeroize::{Zeroize, ZeroizeOnDrop};
+
+/// CLI arguments
+#[derive(Parser, Debug, Zeroize, ZeroizeOnDrop)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The mnemonic to decode.
+    #[arg(short, long)]
+    mnemonic: String,
+    /// The index from which to derive the keystore.
+    #[arg(short, long)]
+    index: u32,
+    /// The password to use to encrypt the resulting keystore.
+    #[arg(short, long)]
+    password: String,
+    /// The directory in which to output the keystore file.
+    #[arg(short, long)]
+    output_dir: String,
+}
+
+fn main() {
+    if let Err(e) = main_fallible() {
+        eprintln!("{}", e);
+        exit(-1);
+    }
+}
+
+fn main_fallible() -> Result<(), String> {
+    let args = Args::try_parse().map_err(|e| e.to_string())?;
+
+    let output_dir = PathBuf::from_str(&args.output_dir)
+        .map_err(|e| format!("Unable to parse output-dir: {e}"))?;
+    if !output_dir.exists() {
+        return Err(format!(
+            "Output directory does not exist: {:?}",
+            args.output_dir
+        ));
+    }
+
+    let index = args.index;
+
+    let wallet = MnemonicBuilder::<English>::default()
+        .phrase(&args.mnemonic)
+        .index(index)
+        .map_err(|e| format!("Invalid index {index}: {e}"))?
+        .build()
+        .map_err(|e| format!("Unable to build keystore from mnemonic: {e}"))?;
+
+    let mut rng = thread_rng();
+    PrivateKeySigner::encrypt_keystore(
+        &output_dir,
+        &mut rng,
+        wallet.to_bytes(),
+        &args.password,
+        None,
+    )
+    .map_err(|e| format!("Unable to build keystore: {e}"))?;
+
+    Ok(())
+}
